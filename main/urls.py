40eb8faa03146bf930b122f2e808138fb78b10ac
Abdulll02 <zeltyjvelosiped@gmail.com>
2024-02-06 17:33:22 +0300

    The main app is ready, pages index and about

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+"""Это url-адреса, отдельно для приложения main"""
+
+from django.urls import path
+
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+]
