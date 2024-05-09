8e74e29dfa8e0f95d1faf0af514edf8bc4b753c2
Abdulll02 <zeltyjvelosiped@gmail.com>
2024-05-09 19:45:55 +0300

    After adding fixtures for goods

--- goods/models.py
+++ goods/models.py
@@ -1,16 +1,37 @@
-from os import name
-from tabnanny import verbose
 from typing import Any
 from django.db import models
 
 # Create your models here.
 
 class Categories(models.Model):
-    name: Any = models.CharField(max_length=150, unique=True, verbose_name='Название')
-    slug: Any = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
     class Meta:
         db_table: str = 'category'
         verbose_name: str = 'Категорию'
         verbose_name_plural: str = 'Категории'
+
+    def __str__(self):
+        return f'{self.name}'
+            
+
+class Products(models.Model):
+    name: Any = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug: Any = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00 , max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(default=0.00 , max_digits=4, decimal_places=2, verbose_name='Скидка в процентах')
+    quatity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+
+    class Meta:
+        db_table: str = 'product'
+        verbose_name: str = 'Продукт'
+        verbose_name_plural: str = 'Продукты'
         
+    def __str__(self):
+        return f'{self.name} Количество - {self.quatity}'
+            
