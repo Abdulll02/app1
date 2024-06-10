11a5cb97dabf2b1f2e5ba806aba7ba15c8810266
Abdulll02 <zeltyjvelosiped@gmail.com>
2024-06-10 17:13:12 +0300

    Second pagination options - GET params

--- goods/urls.py
+++ goods/urls.py
@@ -2,12 +2,12 @@
 
 from django.urls import path
 
+
 from goods import views
 
 app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('<slug:category_slug>/<int:page>', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
