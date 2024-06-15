ab55320afe61b1c581fca3759904345b2a43e3bd
Abdulll02 <zeltyjvelosiped@gmail.com>
2024-06-15 20:17:13 +0300

    Simple search created

--- goods/views.py
+++ goods/views.py
@@ -3,16 +3,22 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
+from goods.utils import q_search
 
 
-def catalog(request, category_slug,):
+def catalog(request, category_slug=None):
+    """Контроллер для каталога"""
 
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
+
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
@@ -35,7 +41,7 @@ def catalog(request, category_slug,):
 
 
 def product(request, product_slug):
-
+    """Контроллер уже для каждой картчоки отдельно"""
     product = Products.objects.get(slug=product_slug)
 
     context = {
